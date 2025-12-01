b1b1890e1ad2d248ac5f344fa3d0f3f53cdc54ed
RerissonHRS <rerisson.rodrigues@hotmail.com>
2025-12-01 14:53:15 -0300

    Primeira versão do site

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='.', static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
